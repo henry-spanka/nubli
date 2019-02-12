2df987dffb319d6208d49d16cd478c9bffd27076
Henry Spanka <henry@spanka.eu>
2019-02-12 18:56:17 +0100

    fix device discovery

--- src/lib/smartLockPeripheralFilter.ts
+++ src/lib/smartLockPeripheralFilter.ts
@@ -1,9 +1,24 @@
 import { PeripheralFilter } from "./peripheralFilter";
+import { SmartLock } from "./smartLock";
 
 export class SmartLockPeripheralFilter implements PeripheralFilter {
     handle(peripheral: import("noble").Peripheral): boolean {
-        let name: string = peripheral.advertisement.localName;
+        let data: Buffer = peripheral.advertisement.manufacturerData;
 
-        return name !== undefined && peripheral.advertisement.localName.slice(0, 3) == "Nuk";
+        if (data.length == 25) {
+            let type: number = data.readUInt8(2);
+            let dataLength: number = data.readUInt8(3);
+            
+            // 0x02 == iBeacon
+            if (type == 2 && dataLength == 21) {
+                let serviceUuid: string = data.slice(4, 20).toString('hex');
+
+                if (serviceUuid == SmartLock.NUKI_SERVICE_UUID) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
     }
 }
